229e415bb6944e160d5310871bcce8631a79b0b0
Ghianda22 <valelore98@gmail.com>
2023-05-29 12:33:19 +0200

    Fixed a bug, added a count method and a print method
    Array was filled by reference, not by value.
    Count method counts all the lights that are on.
    Print method is used to visualize the lights

--- src/GridOfLights.ts
+++ src/GridOfLights.ts
@@ -11,8 +11,19 @@ const COLUMNS_NUMBER: number = 1000;
  *
  * -> grid[row][column]
  * */
-export default class GridOfLights{
-    #_gridOfLights: boolean[][] = Array(ROWS_NUMBER).fill(Array(COLUMNS_NUMBER).fill(false));
+export default class GridOfLights {
+    // #_gridOfLights: boolean[][] = Array(ROWS_NUMBER).fill(Array(COLUMNS_NUMBER).fill(false));
+    #_gridOfLights: boolean[][] = [];
+
+    constructor() {
+        for (let rowIndex = 0; rowIndex < ROWS_NUMBER; rowIndex++) {
+            this.#_gridOfLights.push([]);
+            for (let columnIndex = 0; columnIndex < COLUMNS_NUMBER; columnIndex++) {
+                this.#_gridOfLights[rowIndex].push(false)
+            }
+        }
+    }
+
 
     get gridOfLights(): boolean[][] {
         return this.#_gridOfLights;
@@ -22,27 +33,56 @@ export default class GridOfLights{
         this.#_gridOfLights = value;
     }
 
-    turnOn(fisrtColumnIndex: number, firstRowIndex: number,  lastColumnIndex: number, lastRowIndex: number) {
-        for (let i = firstRowIndex; i <= lastRowIndex; i++) {
-            for (let j = fisrtColumnIndex; j <= lastColumnIndex; j++) {
-                this.gridOfLights[i][j] = true;
+    turnOn(firstColumnIndex: number, firstRowIndex: number, lastColumnIndex: number, lastRowIndex: number) {
+        for (let rowIndex = firstRowIndex; rowIndex <= lastRowIndex; rowIndex++) {
+            for (let columnIndex = firstColumnIndex; columnIndex <= lastColumnIndex; columnIndex++) {
+                this.gridOfLights[rowIndex][columnIndex] = true;
             }
         }
     }
 
-    turnOff(fisrtColumnIndex: number, firstRowIndex: number,  lastColumnIndex: number, lastRowIndex: number) {
-        for (let i = firstRowIndex; i <= lastRowIndex; i++) {
-            for (let j = fisrtColumnIndex; j <= lastColumnIndex; j++) {
-                this.gridOfLights[i][j] = false;
+    turnOff(firstColumnIndex: number, firstRowIndex: number, lastColumnIndex: number, lastRowIndex: number) {
+        for (let rowIndex = firstRowIndex; rowIndex <= lastRowIndex; rowIndex++) {
+            for (let columnIndex = firstColumnIndex; columnIndex <= lastColumnIndex; columnIndex++) {
+                this.gridOfLights[rowIndex][columnIndex] = false;
             }
         }
     }
 
-    toggle(fisrtColumnIndex: number, firstRowIndex: number,  lastColumnIndex: number, lastRowIndex: number) {
-        for (let i = firstRowIndex; i <= lastRowIndex; i++) {
-            for (let j = fisrtColumnIndex; j <= lastColumnIndex; j++) {
-                this.gridOfLights[i][j] = !this.gridOfLights[i][j];
+    toggle(firstColumnIndex: number, firstRowIndex: number, lastColumnIndex: number, lastRowIndex: number) {
+        for (let rowIndex = firstRowIndex; rowIndex <= lastRowIndex; rowIndex++) {
+            // console.log(rowIndex + " status: " + this.gridOfLights[rowIndex]);
+            for (let columnIndex = firstColumnIndex; columnIndex <= lastColumnIndex; columnIndex++) {
+                this.gridOfLights[rowIndex][columnIndex] = !this.gridOfLights[rowIndex][columnIndex];
+                this.gridOfLights;
+                // console.log("column " + columnIndex + " row: " + rowIndex + this.gridOfLights[rowIndex][columnIndex]);
+                // console.log("RIGA DOPO: column:" + columnIndex + " row:" + (rowIndex+1) + this.gridOfLights[rowIndex+1][columnIndex]);
             }
         }
+    }
+
+    printGrid() {
+        let gridAsString: string = "";
+        for (let rowIndex = 0; rowIndex < ROWS_NUMBER; rowIndex++) {
+            for (let columnIndex = 0; columnIndex < COLUMNS_NUMBER; columnIndex++) {
+                gridAsString = gridAsString.concat(this.gridOfLights[rowIndex][columnIndex] ? "0" : "-");
+            }
+            gridAsString = gridAsString.concat("\n");
+        }
+
+        console.log(gridAsString);
+
+    }
+
+    countLightsOn(): number {
+        let numberOfLightsOn: number = 0;
+        for (let rowIndex = 0; rowIndex < ROWS_NUMBER; rowIndex++) {
+            for (let columnIndex = 0; columnIndex < COLUMNS_NUMBER; columnIndex++) {
+                if(this.gridOfLights[rowIndex][columnIndex]){
+                    numberOfLightsOn++;
+                }
+            }
+        }
+        return numberOfLightsOn;
     }
 }
